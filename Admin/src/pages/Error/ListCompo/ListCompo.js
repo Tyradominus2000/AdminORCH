b9acf2706bf940bd8dd5f4fc8e9a46c28adcbe3b
Tyradominus2000 <obszynski.tristan@outlook.com>
2023-06-08 14:11:42 +0200

    feat : form

--- Admin/src/pages/Error/ListCompo/ListCompo.js
+++ Admin/src/pages/Error/ListCompo/ListCompo.js
@@ -8,7 +8,8 @@ import { useForm } from "react-hook-form";
 
 export default function ListCompo() {
   const [loader, setLoader] = useState(true);
-  const [component, setComponent] = useState([]);
+  const [components, setComponents] = useState([]);
+  const [component, setComponent] = useState();
 
   const yupSchema = yup.object({
     name: yup.string(),
@@ -38,13 +39,70 @@ export default function ListCompo() {
     maxTemp: yup.string(),
   });
 
+  const defaultValues = {
+    name: "",
+    codename: "",
+    price: "",
+    date: "",
+    brand: "",
+    socket: "",
+    litho: "",
+    core: "",
+    thread: "",
+    cache: "",
+    clock: "",
+    maxclock: "",
+    bus: "",
+    memory: "",
+    maxMemory: "",
+    bandwithMemory: "",
+    channelMemory: "",
+    ECCMemory: "",
+    itg: "",
+    itgFreq: "",
+    itgMaxFreq: "",
+    itgMaxMem: "",
+    itg4k: "",
+    tdp: "",
+    maxTemp: "",
+  };
+
   const {
     register,
+    reset,
     handleSubmit,
-    setError,
-    clearErrors,
-    formState: { errors },
+    // setError,
+    // clearErrors,
+    // formState: { errors },
   } = useForm({
+    defaultValues,
+    values: {
+      name: component ? component.ComponentName : null,
+      codename: component ? component.CPUcodeName : null,
+      price: component ? component.CPUprice : null,
+      date: component ? component.CPUreleaseDate : null,
+      brand: component ? component.CPUbrand : null,
+      socket: component ? component.CPUSockets : null,
+      litho: component ? component.CPUlithograph : null,
+      core: component ? component.CPUcoreCount : null,
+      thread: component ? component.CPUthreadCount : null,
+      cache: component ? component.CPUcache : null,
+      clock: component ? component.CPUclockSpeed : null,
+      maxclock: component ? component.CPUmaxClockSpeed : null,
+      bus: component ? component.CPUbus : null,
+      memory: component ? component.CPUtypeMemory : null,
+      maxMemory: component ? component.CPUmaxMemory : null,
+      bandwithMemory: component ? component.CPUmaxMemoryBandwidth : null,
+      channelMemory: component ? component.CPUnumberMemoryChannel : null,
+      ECCMemory: component ? component.CPUsupportECCMemory : null,
+      itg: component ? component.CPUitgdGraphic : null,
+      itgFreq: component ? component.CPUitgdGraphicFreq : null,
+      itgMaxFreq: component ? component.CPUitgdGraphicMaxFreq : null,
+      itgMaxMem: component ? component.CPUitgdGraphicMaxMemory : null,
+      itg4k: component ? component.CPUitgdGraphicSupport4K : null,
+      tdp: component ? component.CPUmaxTDP : null,
+      maxTemp: component ? component.CPUmaxTemp : null,
+    },
     resolver: yupResolver(yupSchema),
   });
 
@@ -53,22 +111,73 @@ export default function ListCompo() {
     setLoader(true);
     async function Component() {
       GetComponent().then((c) => {
-        setComponent(c);
+        setComponents(c);
         setLoader(false);
       });
     }
     Component();
   }, []);
 
-  //   Debug
+  //DEBUG
+  useEffect(() => {
+    console.log(components);
+  }, [components]);
   useEffect(() => {
     console.log(component);
   }, [component]);
+  //DEBUG
 
-  function HandleClick() {}
+  //   To change between focus of the list to the input
+  function handleClick(index) {
+    setComponent(components[index]);
+    const formContainer = document.getElementById("formContainer");
+    formContainer.classList.remove("dnone");
+  }
+//   To chnage between focus of the input for the list
+  function handleClickCancel() {
+    setComponent();
+    const formContainer = document.getElementById("formContainer");
+    formContainer.classList.add("dnone");
+  }
 
+  //   SUBMIT
   async function submit(values) {
     console.log(values);
+    // If the input have the same value as the components
+    if (
+      component.ComponentName.toString() === values.name.toString() &&
+      component.CPUcodeName.toString() === values.codename.toString() &&
+      component.CPUprice.toString() === values.price.toString() &&
+      component.CPUreleaseDate.toString() === values.date.toString() &&
+      component.CPUbrand.toString() === values.brand.toString() &&
+      component.CPUSockets.toString() === values.socket.toString() &&
+      component.CPUlithograph.toString() === values.litho.toString() &&
+      component.CPUcoreCount.toString() === values.core.toString() &&
+      component.CPUthreadCount.toString() === values.thread.toString() &&
+      component.CPUcache.toString() === values.cache.toString() &&
+      component.CPUclockSpeed.toString() === values.clock.toString() &&
+      component.CPUmaxClockSpeed.toString() === values.maxclock.toString() &&
+      component.CPUbus.toString() === values.bus.toString() &&
+      component.CPUtypeMemory.toString() === values.memory.toString() &&
+      component.CPUmaxMemory.toString() === values.maxMemory.toString() &&
+      component.CPUmaxMemoryBandwidth.toString() ===
+        values.bandwithMemory.toString() &&
+      component.CPUsupportECCMemory.toString() ===
+        values.ECCMemory.toString() &&
+      component.CPUitgdGraphic.toString() === values.itg.toString() &&
+      component.CPUitgdGraphicFreq.toString() === values.itgFreq.toString() &&
+      component.CPUitgdGraphicMaxFreq.toString() ===
+        values.itgMaxFreq.toString() &&
+      component.CPUitgdGraphicSupport4K.toString() ===
+        values.itg4k.toString() &&
+      component.CPUmaxTDP.toString() === values.tdp.toString() &&
+      component.CPUmaxTemp.toString() === values.maxTemp.toString()
+    ) {
+      console.log("TEST");
+    }else{
+
+    }
+    console.log(component);
   }
 
   return (
@@ -79,12 +188,12 @@ export default function ListCompo() {
         </div>
       ) : (
         <>
-          <div>
+          <div id="list">
             <h2>ListCompo</h2>
-            {component ? (
+            {components ? (
               <>
                 <ul className={`d-flex flex-column ${styles.ListContainer}`}>
-                  {component.map((c) => (
+                  {components.map((c, i) => (
                     <li key={c.idComponent} className="d-flex flex-fill my20">
                       <img
                         className="mx10"
@@ -132,7 +241,7 @@ export default function ListCompo() {
                       <button
                         className="mx10"
                         type="button"
-                        onClick={HandleClick}
+                        onClick={() => handleClick(i)}
                       >
                         Edit
                       </button>
@@ -144,77 +253,198 @@ export default function ListCompo() {
               <>Pas de components</>
             )}
           </div>
-          <div
-            className={`${styles.Insert} d-flex flex-column justify-content-center align-items-center`}
-          >
-            <form onSubmit={handleSubmit(submit)}>
-              <div>
-                <img />
-                <h2>Id : hfidhfidh</h2>
-              </div>
-              <div
-                className={`d-flex justify-content-center align-items-center`}
-              >
-                <div className="d-flex flex-column">
-                  <label>Name</label>
-                  <input type="text" {...register("name")}></input>
-                  <label>CodeName</label>
-                  <input type="text" {...register("codename")}></input>
-                  <label>Price</label>
-                  <input type="text" {...register("price")}></input>
-                  <label>Date</label>
-                  <input type="text" {...register("date")}></input>
-                  <label>Brand</label>
-                  <input type="text" {...register("brand")}></input>
-                  <label>Socket</label>
-                  <input type="text" {...register("socket")}></input>
-                  <label>Lithoragphie</label>
-                  <input type="text" {...register("litho")}></input>
-                  <label>CoreCount</label>
-                  <input type="text" {...register("core")}></input>
-                  <label>ThreadCount</label>
-                  <input type="text" {...register("thread")}></input>
-                  <label>Cache</label>
-                  <input type="text" {...register("cache")}></input>
-                  <label>ClockSpeed</label>
-                  <input type="text" {...register("clock")}></input>
-                  <label>Max ClockSpeed</label>
-                  <input type="text" {...register("maxclock")}></input>
-                  <label>Bus</label>
-                  <input type="text" {...register("bus")}></input>
+          <div className={`${styles.FormContainer} dnone`} id="formContainer">
+            <div
+              id="form"
+              className={`${styles.Insert} d-flex flex-column justify-content-center align-items-center`}
+            >
+              <form onSubmit={handleSubmit(submit)}>
+                <div>
+                  <img />
+                  <h2>Id : hfidhfidh</h2>
                 </div>
-                <div className="d-flex flex-column mx10">
-                  <label>Memory Type</label>
-                  <input type="text" {...register("memory")}></input>
-                  <label>Max Memory</label>
-                  <input type="text" {...register("maxMemory")}></input>
-                  <label>Memory Bandwith</label>
-                  <input type="text" {...register("bandwithMemory")}></input>
-                  <label>Number of Memory Channel</label>
-                  <input type="text" {...register("channelMemory")}></input>
-                  <label>Support ECC memory</label>
-                  <input type="text" {...register("ECCMemory")}></input>
-                  <label>Integraded Graphic</label>
-                  <input type="text" {...register("itg")}></input>
-                  <label>Integraded Graphic Freq</label>
-                  <input type="text" {...register("itgFreq")}></input>
-                  <label>Integraded Graphic Max Freq</label>
-                  <input type="text" {...register("itgMaxFreq")}></input>
-                  <label>Integraded Graphic Max memory</label>
-                  <input type="text" {...register("itgMaxMem")}></input>
-                  <label>Integraded Graphic 4k support </label>
-                  <input type="text" {...register("itg4k")}></input>
-                  <label>TDP</label>
-                  <input type="text" {...register("tdp")}></input>
-                  <label>Max Temp</label>
-                  <input type="text" {...register("maxTemp")}></input>
+                <div
+                  className={`d-flex justify-content-center align-items-center`}
+                >
+                  <div className="d-flex flex-column">
+                    <label>Name</label>
+                    <input
+                      type="text"
+                      {...register("name")}
+                      defaultValue={component ? component.ComponentName : null}
+                    ></input>
+                    <label>CodeName</label>
+                    <input
+                      type="text"
+                      {...register("codename")}
+                      defaultValue={component ? component.CPUcodeName : null}
+                    ></input>
+                    <label>Price</label>
+                    <input
+                      type="text"
+                      {...register("price")}
+                      defaultValue={component ? component.CPUprice : null}
+                    ></input>
+                    <label>Date</label>
+                    <input
+                      type="text"
+                      {...register("date")}
+                      defaultValue={component ? component.CPUreleaseDate : null}
+                    ></input>
+                    <label>Brand</label>
+                    <input
+                      type="text"
+                      {...register("brand")}
+                      defaultValue={component ? component.CPUbrand : null}
+                    ></input>
+                    <label>Socket</label>
+                    <input
+                      type="text"
+                      {...register("socket")}
+                      defaultValue={component ? component.CPUSockets : null}
+                    ></input>
+                    <label>Lithoragphie</label>
+                    <input
+                      type="text"
+                      {...register("litho")}
+                      defaultValue={component ? component.CPUlithograph : null}
+                    ></input>
+                    <label>CoreCount</label>
+                    <input
+                      type="text"
+                      {...register("core")}
+                      defaultValue={component ? component.CPUcoreCount : null}
+                    ></input>
+                    <label>ThreadCount</label>
+                    <input
+                      type="text"
+                      {...register("thread")}
+                      defaultValue={component ? component.CPUthreadCount : null}
+                    ></input>
+                    <label>Cache</label>
+                    <input
+                      type="text"
+                      {...register("cache")}
+                      defaultValue={component ? component.CPUcache : null}
+                    ></input>
+                    <label>ClockSpeed</label>
+                    <input
+                      type="text"
+                      {...register("clock")}
+                      defaultValue={component ? component.CPUclockSpeed : null}
+                    ></input>
+                    <label>Max ClockSpeed</label>
+                    <input
+                      type="text"
+                      {...register("maxclock")}
+                      defaultValue={
+                        component ? component.CPUmaxClockSpeed : null
+                      }
+                    ></input>
+                    <label>Bus</label>
+                    <input
+                      type="text"
+                      {...register("bus")}
+                      defaultValue={component ? component.CPUbus : null}
+                    ></input>
+                  </div>
+                  <div className="d-flex flex-column mx10">
+                    <label>Memory Type</label>
+                    <input
+                      type="text"
+                      {...register("memory")}
+                      defaultValue={component ? component.CPUtypeMemory : null}
+                    ></input>
+                    <label>Max Memory</label>
+                    <input
+                      type="text"
+                      {...register("maxMemory")}
+                      defaultValue={component ? component.CPUmaxMemory : null}
+                    ></input>
+                    <label>Memory Bandwith</label>
+                    <input
+                      type="text"
+                      {...register("bandwithMemory")}
+                      defaultValue={
+                        component ? component.CPUmaxMemoryBandwidth : null
+                      }
+                    ></input>
+                    <label>Number of Memory Channel</label>
+                    <input
+                      type="text"
+                      {...register("channelMemory")}
+                      defaultValue={
+                        component ? component.CPUnumberMemoryChannel : null
+                      }
+                    ></input>
+                    <label>Support ECC memory</label>
+                    <input
+                      type="text"
+                      {...register("ECCMemory")}
+                      defaultValue={
+                        component ? component.CPUsupportECCMemory : null
+                      }
+                    ></input>
+                    <label>Integraded Graphic</label>
+                    <input
+                      type="text"
+                      {...register("itg")}
+                      defaultValue={component ? component.CPUitgdGraphic : null}
+                    ></input>
+                    <label>Integraded Graphic Freq</label>
+                    <input
+                      type="text"
+                      {...register("itgFreq")}
+                      defaultValue={
+                        component ? component.CPUitgdGraphicFreq : null
+                      }
+                    ></input>
+                    <label>Integraded Graphic Max Freq</label>
+                    <input
+                      type="text"
+                      {...register("itgMaxFreq")}
+                      defaultValue={
+                        component ? component.CPUitgdGraphicFreq : null
+                      }
+                    ></input>
+                    <label>Integraded Graphic Max memory</label>
+                    <input
+                      type="text"
+                      {...register("itgMaxMem")}
+                      defaultValue={
+                        component ? component.CPUitgdGraphicMaxMemory : null
+                      }
+                    ></input>
+                    <label>Integraded Graphic 4k support </label>
+                    <input
+                      type="text"
+                      {...register("itg4k")}
+                      defaultValue={
+                        component ? component.CPUitgdGraphicSupport4K : null
+                      }
+                    ></input>
+                    <label>TDP</label>
+                    <input
+                      type="text"
+                      {...register("tdp")}
+                      defaultValue={component ? component.CPUmaxTDP : null}
+                    ></input>
+                    <label>Max Temp</label>
+                    <input
+                      type="text"
+                      {...register("maxTemp")}
+                      defaultValue={component ? component.CPUmaxTemp : null}
+                    ></input>
+                  </div>
                 </div>
-              </div>
-              <div className="d-flex flex-content-end">
-                <button type="button">Cancel</button>
-                <button>Save</button>
-              </div>
-            </form>
+                <div className="d-flex flex-content-end">
+                  <button type="button" onClick={handleClickCancel}>
+                    Cancel
+                  </button>
+                  <button>Save</button>
+                </div>
+              </form>
+            </div>
           </div>
         </>
       )}
